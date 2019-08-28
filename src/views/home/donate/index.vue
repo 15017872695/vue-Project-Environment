2b151fe183b9885446eb21e387515ba0afd5d7c9
小余 <15017872695@163.com>
2019-08-28 17:47:21 +0800

    完成支持作者页

--- src/views/home/donate/index.vue
+++ src/views/home/donate/index.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
-        支持作者
+        <div class="container">
+            <div class="plugins-tips">
+                如果该框架对你有帮助，那就请作者喝杯饮料吧！加微信号yhq15017872695探讨问题。<strong>记得填写添加来由哦~</strong>
+            </div>
+            <div><img src="../../../assets/img/二维码.jpg" style='width:20%;margin-top:10px;'></div>
+        </div>
     </div>
 </template>
 <script>
--- src/views/home/form/basicForm/index.vue
+++ src/views/home/form/basicForm/index.vue
@@ -9,8 +9,8 @@
                         </el-form-item>
                         <el-form-item label="活动区域">
                             <el-select v-model="form.region" placeholder="请选择活动区域">
-                                <el-option label="区域一" value="shanghai"></el-option>
-                                <el-option label="区域二" value="beijing"></el-option>
+                                <el-option label="深圳市" value="shanghai"></el-option>
+                                <el-option label="东莞市" value="beijing"></el-option>
                             </el-select>
                         </el-form-item>
                         <el-form-item label="活动时间">
